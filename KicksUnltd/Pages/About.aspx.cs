aaad89cbe725bf564bde30d1c0ce3559005eaad5
mcfarlanda <mcfarland_andre@columbusstate.edu>
2015-04-16 01:10:44 -0400

    reup
    added About, Privacy & Social pages inside Pages folder. Finished master
    page along with its css. put more images in Image folder.

--- KicksUnltd/Pages/About.aspx.cs
+++ KicksUnltd/Pages/About.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Order : System.Web.UI.Page
+public partial class Pages_About : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
